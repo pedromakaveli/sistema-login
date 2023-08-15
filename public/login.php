09ddb2a73ec75dfdc77e4ca23f2f64fefed10281
guihkx <626206+guihkx@users.noreply.github.com>
2023-08-15 00:40:21 -0300

    Unifica da página de login
    Torna mais fácil a validação do formulário e exibição de erros para os
    usuários.

--- public/login.php
+++ public/login.php
@@ -1,9 +1,44 @@
+<?php
+// iniciando sessão
+
+session_start();
+
+if (isset($_POST['email']) && isset($_POST['senha'])) {
+    // acessou
+
+    include_once('config.php');
+
+    $email = trim($_POST['email']);
+    $password = trim($_POST['senha']);
+    $errors = [];
+
+    // Verificando se os parametros existem na db
+    $stmt = $connect->prepare('SELECT email, senha FROM usuarios WHERE email = :email');
+    $stmt->bindValue('email', $email, PDO::PARAM_STR);
+
+    if ($stmt->execute()) {
+        $user_info = $stmt->fetch(PDO::FETCH_OBJ);
+        if (isset($user_info->email) && isset($user_info->senha)) {
+            if (password_verify($password, $user_info->senha)) {
+                $_SESSION['email'] = $email; // caso encontre os dados na db, o email é armazenado na sessão
+                $_SESSION['senha'] = $password; // caso encontre os dados na db, a senha é armazenada na sessão
+                header('Location: sistema.php');
+                exit;
+            }
+        }
+        $errors[] = 'Email ou senha incorretos';
+    }
+    else {
+        $errors[] = 'Falha ao conectar-se ao banco de dados!';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de cadastro</title>
+    <title>Login</title>
 
     <style>
         body {
@@ -61,12 +96,20 @@
 
     <h1>Faça seu login</h1>
 
-    <form action="testlogin.php" method="POST">
+    <form action="login.php" method="POST">
         <label for="">Seu email</label>
-        <input required placeholder="Seu email" type="email" name="email" id="">
+        <input required placeholder="Seu email" type="email" name="email" id="" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
 
         <label for="">Senha</label>
         <input type="password" name="senha" id="">
+
+        <?php
+            if (!empty($errors)) {
+                foreach($errors as $error) {
+                    echo "<span style='background: red; padding: 5px 15px; color: #ffff'><b>{$error}</b></span>" . PHP_EOL;
+                }
+            }
+        ?>
 
         <input required class="btn-enviar" type="submit" name="submit" value="Enviar">
 
